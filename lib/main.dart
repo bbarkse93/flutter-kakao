969f06ff0c223214540ac950e3c456699e65cb8a
bbarkse93 <bbarkse93@gmail.com>
2023-10-15 17:10:10 +0900

    BottomNavigationBar 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:kakao_app/pages/chat_page.dart';
+import 'package:kakao_app/pages/friend_page.dart';
+import 'package:kakao_app/pages/more_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,11 +19,47 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
   @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          FriendPage(),
+          ChatPage(),
+          MorePage(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.black26,
+        backgroundColor: Colors.grey[100],
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.profile_circled), label: ""),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.chat_bubble_2_fill), label: ""),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.ellipsis), label: ""),
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+    );
   }
 }
